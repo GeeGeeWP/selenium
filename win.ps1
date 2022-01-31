555c831a6b90ad8729c8019f42355b1663b5385b
GeeGeeWP <34427577+GeeGeeWP@users.noreply.github.com>
2022-01-30 22:25:35 -0500

    Update win.ps1

--- win.ps1
+++ win.ps1
@@ -1,3 +1,7 @@
+echo "Startup Started" > .\Desktop\Status.txt
+
+$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
+
 echo "Getting Selenium" >> .\Desktop\Status.txt
 
 Invoke-WebRequest -Uri https://chromedriver.storage.googleapis.com/97.0.4692.71/chromedriver_win32.zip -OutFile .\Desktop\sel.zip
